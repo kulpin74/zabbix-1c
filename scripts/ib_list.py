d77bcf82e62226804b55227a642b32b766f7faab
Alexey Kulpin <kulpin@sl-tech.ru>
2020-08-25 15:44:11 +0300

    - удалены exe файлы - внесены изменения для возможности использования под Linux

--- scripts/ib_list.py
+++ scripts/ib_list.py
@@ -1,24 +1,30 @@
 import subprocess
 import json
+import os
 
 
-def getDictFromString(txtBlock):
+C_PAGE = 'cp866' if os.name == 'nt' else ''
+
+
+def get_dict_from_string(txt_block):
     dict_el = {}
-    for i in txtBlock.split('\r\n'):
-        dict_el = {**dict_el, **dict([[j.strip().strip('"') for j in i.split(' :')]])}
+    for i in txt_block.split('\r\n'):
+        for j in i.split('\n'):
+            key, value = j.split(':')
+            dict_el.update({key.strip(): value.strip().strip('"')})
     return dict_el
 
 
-cluster_res = subprocess.check_output(['rac', 'cluster', 'list']).decode('cp866').strip()
+cluster_res = subprocess.check_output(['rac', 'cluster', 'list']).decode(C_PAGE).strip()
 ib_list = []
 for cluster in cluster_res.split('\r\n\r\n'):
-    curr_cluster = getDictFromString(cluster)
-    ib_res = subprocess.check_output(['rac', 'infobase', 'summary', 'list', '--cluster='+curr_cluster['cluster']]).decode('cp866').strip()
+    curr_cluster = get_dict_from_string(cluster)
+    ib_res = subprocess.check_output(['rac', 'infobase', 'summary', 'list', '--cluster='+curr_cluster['cluster']]).decode(C_PAGE).strip()
     ib_res = ib_res.replace("infobase", "{#INFOBASE}")
     ib_res = ib_res.replace("name", "{#NAME}")
     ib_res = ib_res.replace("descr", "{#DESCR}")
     for ib in ib_res.split('\r\n\r\n'):
-        ib_list.append({**getDictFromString(ib),
+        ib_list.append({**get_dict_from_string(ib),
                         "{#CLUSTER}": curr_cluster['cluster'],
                         "{#CLUSTER_NAME}": curr_cluster['name']})
     ib_list.append({"{#INFOBASE}": "all_infobases",
